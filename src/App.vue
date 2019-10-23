557ba3e241f1c3208b24e72c3f0dc002a8cdd90c
jiang <2676925882@qq.com>
2019-10-23 16:36:24 +0800

    commit project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+export default {
+  name: "App",
+  components: {
+    Header,
+  }
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  border: none;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="head">
+        <div class="head-left"></div>
+        <div class="head-right">
+            <a href="">关于</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+    .head {
+        height: 60px;
+        background: #5a5555;
+    }
+
+    .head-right {
+        float: right;
+        margin-right: 100px;
+    }
+
+    .head-right a {
+        line-height: 60px;
+        color: #cbc9c9;
+        font-size: 20px;
+        text-decoration: none;
+    }
+</style>
